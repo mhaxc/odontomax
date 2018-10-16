fc39289be3ac377adaf90a8e4547156fe1e8a2d1
mhaxc <mhaxcorp@outlook.com>
2018-10-16 17:30:02 -0300

    dentista quase pronto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="https://fonts.google.com/specimen/Roboto+Slab" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
